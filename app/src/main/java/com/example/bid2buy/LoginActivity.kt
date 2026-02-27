1475f45505793b6e5cd9a6c5a2ad756ad8d88d44
Gal Kosover <galkosover0503@gmail.com>
2026-02-27 16:29:01 +0200

    add ui for home & login pages

--- app/src/main/java/com/example/bid2buy/LoginActivity.kt
+++ app/src/main/java/com/example/bid2buy/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.bid2buy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val backArrow = findViewById<ImageView>(R.id.back_arrow)
+        backArrow.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
